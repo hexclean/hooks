13e0151ce3041a34368c62f9651b05eb284ae7e3
hexclean <erdosjozsef20@gmail.com>
2022-01-08 20:08:12 +0200

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const increaseHandler = () => {};
+  const decreaseHandler = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter-container">
+      <h1>Counter</h1>
+      <div className="counter">0</div>
+      <div className="counter-btn">
+        <div>
+          <button className="decrease-btn">Decrease</button>
+        </div>
+        <div>
+          <button className="increase-btn">Increase</button>
+        </div>
+      </div>
     </div>
   );
 }
